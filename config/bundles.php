0965b459d113b370389b88294670922760da9bfd
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-05-09 13:30:01 +0000

    Add trikoder/oauth2-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,8 @@ return [
     Maba\Bundle\GentleForceBundle\MabaGentleForceBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\SymfonyBundlesJsonRequestBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Trikoder\Bundle\OAuth2Bundle\TrikoderOAuth2Bundle::class => ['all' => true],
 ];
