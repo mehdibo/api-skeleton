a9e1612b23a641272916716b38e2153f0e9e25cf
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-05-09 13:09:53 +0000

    Add symfony/monolog-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Maba\Bundle\GentleForceBundle\MabaGentleForceBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\SymfonyBundlesJsonRequestBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
