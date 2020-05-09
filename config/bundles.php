4a86cb3f601a563814e5061893c671d8c25c2872
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-05-09 13:33:26 +0000

    Add symfony/orm-pack

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Trikoder\Bundle\OAuth2Bundle\TrikoderOAuth2Bundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
