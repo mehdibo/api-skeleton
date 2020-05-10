9ab97546ee4710d7315d9eccc41a8580816ef348
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-05-10 12:43:28 +0000

    Create UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture implements FixtureGroupInterface
+{
+    public const TOTAL_COUNT = 40;
+
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+        for ($i = 0; $i < self::TOTAL_COUNT; $i++)
+        {
+            $email = $faker->email;
+            $user = new User();
+            $user->setEmail($email)
+                ->setPassword($this->passwordEncoder->encodePassword($user, $email));
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['users'];
+    }
+}
