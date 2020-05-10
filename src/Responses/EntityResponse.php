27f1e952e9bdd7b662bcb8a2f98a5cd3eac709a3
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-05-10 13:52:05 +0000

    Fix PHPStan for EntityResponse

--- src/Responses/EntityResponse.php
+++ src/Responses/EntityResponse.php
@@ -9,6 +9,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class EntityResponse extends JsonResponse
 {
 
+    /**
+     * EntityResponse constructor.
+     * @param string $json
+     * @param int $status
+     * @param string[] $headers
+     */
     public function __construct(string $json, int $status = 200, array $headers = [])
     {
         parent::__construct($json, $status, $headers, TRUE);
