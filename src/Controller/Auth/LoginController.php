8426c01ccfe2bc7d8dd2f620939db7ab400ef4be
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-05-22 05:31:32 +0000

    Use common errors trans key in LoginController

--- src/Controller/Auth/LoginController.php
+++ src/Controller/Auth/LoginController.php
@@ -23,7 +23,7 @@ class LoginController extends AbstractController
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         if ($error)
-            $error = 'login.errors.'.$error->getMessageKey();
+            $error = 'common.errors.'.$error->getMessageKey();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
